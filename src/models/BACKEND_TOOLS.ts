587d95311a4ee28a918b9c0541c69e7376e1ddff
YoccoDante <hi.seresx@gmail.com>
2024-01-24 13:23:43 -0500

    adding debug switch

--- src/models/BACKEND_TOOLS.ts
+++ src/models/BACKEND_TOOLS.ts
@@ -1,4 +1,5 @@
-export enum BACKEND_TOOLS {
-    API_URI='http://127.0.0.1:8000',
-    ENTERPRISE_ID = '656a1751cbd9aaa41bd80b60'
+const isDebug = false
+export const BACKEND_TOOLS = {
+    API_URI: isDebug? 'http://127.0.0.1:8000' : 'https://api.bluestpeak.com',
+    ENTERPRISE_ID:'656a1751cbd9aaa41bd80b60'
 }
