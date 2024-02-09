c547349eb6c9b0a59da036ff53ef8b3854ff9443
YoccoDante <hi.seresx@gmail.com>
2024-02-09 18:56:29 -0500

    style: changing display for pictures in Carousel Component

--- src/models/BACKEND_TOOLS.ts
+++ src/models/BACKEND_TOOLS.ts
@@ -1,4 +1,4 @@
-const isDebug = false
+const isDebug = true
 export const BACKEND_TOOLS = {
     API_URI: isDebug? 'http://127.0.0.1:8000' : 'https://api.bluestpeak.com',
     ENTERPRISE_ID:'656a1751cbd9aaa41bd80b60'
